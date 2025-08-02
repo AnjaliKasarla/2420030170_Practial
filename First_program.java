5e10756bf7f01e038c50c38da760a16dc1f76039
AnjaliKasarla <2420030170@klh.edu.in>
2025-08-02 09:32:44 +0530

    Added uploaded file to practical

---
+++ First_program.java
@@ -0,0 +1,27 @@
+package my_project;
+
+public class First_program {
+public static void main(String args[]) {
+	String name="Anjali";
+	String dept="CSE";
+	long roll_no;
+	roll_no=2420030170L;
+	//long phn_no;
+	long phn_no=3674924722L;
+	String add="HYD";
+	/*int a=2;
+	char b='a';
+	double c=4.5;
+	float d=7.88f;
+	String s="hello this is klh";*/
+	/*System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(s);*/
+   //System.out.println(s+","+a+","+b+","+c+","+d);
+	//System.out.printf("integer %d%ncharacter  %c/ndouble %f/nstring %s",a,b,c,s);
+	System.out.printf("name %s%ndept %s%nroll_no %d%nphn_no %d%nadd %s",name,dept,roll_no,phn_no,add);
+}
+}
+ 
